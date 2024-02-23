b6e7608ba02fad937bc80d87e042cd06564a3966
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2024-02-23 22:37:07 +0530

    update socket server link

--- src/providers/socket.jsx
+++ src/providers/socket.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io('http://localhost:8000'),[])
+    const socket = useMemo(() => io('https://silentspeakbackend.onrender.com'),[])
     return(
         <SocketContext.Provider value={socket}>
             {props.children}
