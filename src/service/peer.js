e84bf36e4861789b5ba6357848cc2785649bc4b5
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2024-02-25 00:43:37 +0530

    added trun servers and vers 0.1.1

--- src/service/peer.js
+++ src/service/peer.js
@@ -6,7 +6,16 @@ class PeerService{
                     urls:[
                         "stun:stun.l.google.com:19302",
                         "stun:global.stun.twilio.com:3478",
+                        "stun:openrelay.metered.ca:80",
                     ]
+                },{
+                    "urls": "turn:openrelay.metered.ca:80",
+                    "username": "openrelayproject",
+                    "credential": "openrelayproject"
+                },{
+                    "urls": "turn:openrelay.metered.ca:443",
+                    "username": "openrelayproject",
+                    "credential": "openrelayproject"
                 }]
             })
         }
